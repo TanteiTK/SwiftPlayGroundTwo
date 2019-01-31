585e66c3e5551187fda7839db927f180041c2c6e
CM Student <cmstudent@UHWO-ACM-C-01.local>
2019-01-31 10:21:32 -1000

    Did not finish this class assignment 1/31/2019

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Part 3 start
+var goodGrade="I've been doing all my work"
+//first if statement== is the operator for equality statement, singel = is used for assigning, not checking for equality
+if goodGrade=="I've been doing all my work"
+{
+//keep in mind that whatever
+print("I should be getting a good garde")
+}
+
+//Part3 #2
+
+if goodGrade== "I haven't been doing all my work"
+{
+
+print("I should be getting a good grade")
+}
+part 4
+if goodGrade == "I've been doing all my work"{
+    print(" I should be gettting an A this point")
+}else{
+
+
+
+var gradeA=true
+if gradeA
+{
+    
+    
+    print ("I really like this class")
+}
+else
+
+
+print ("I'm not so sure about coding")
